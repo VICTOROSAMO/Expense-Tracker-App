26c101ad5067963d12e433e11c39f8536c95280b
Osamo <osamovicky2021@gmail.com>
2023-02-06 13:40:33 +0300

    First Commit

---
+++ app/src/main/java/com/osamo/expensetrackingapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.osamo.expensetrackingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.osamo.expensetrackingapp.databinding.ActivityMainBinding;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
